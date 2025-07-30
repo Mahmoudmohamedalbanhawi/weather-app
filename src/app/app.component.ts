5ad45b5c83fb45a1fba61931325bd6aa81ce0292
mahmoudalbanhawi <mahmoudalbanhawi863@gmail.com>
2025-07-30 13:12:46 +0300

    building temp-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './pipes/convert.pipe';
+import { SortPipe } from './pipes/sort.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , DatePipe , SortPipe ,ConvertPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'weather-app';
+  date:Date = new Date();
+  currentTemperatures = {
+    berlin: 4.2749812,
+    newYork: 18.1214,
+    paris: 72.1209001,
+    chicago: 65.0775238,
+  };
+    historicTemperatures = [
+    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
+  ];
+
 }
